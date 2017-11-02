3845f4465b32376f91da39c7e0935601ff8359d9
Nicholas Anderson <nla2121@tc.columbia.edu>
2017-11-02 13:17:11 -0400

    Assignment 3 is all done!

---
+++ Steps to carrying out k-means cluster analysis.R
@@ -0,0 +1,9 @@
+#steps to carrying out cluster analysis
+#1. Load dataset
+#2. Remove missing values, select subset of variables that are usable 
+#3. Scale numeric variables and dichotmous variables
+#4. Do the kmeans fit procedure
+#5. Add the clustered values back to dataframe
+#6. Make good names of df if neccessary
+#7. Use gather function
+#8. 
